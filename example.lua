15d47813efebce625ca0d60083a83d395a85894d
stav <126665568+sstvskids@users.noreply.github.com>
2025-10-09 13:03:32 +1100

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,15 @@
+local lib = loadstring(game:HttpGet(''))()
+local stav = lib.library.CreateWindow('Ballz')
+stav:CreateModule({
+	Name = 'Kill Aura',
+	Function = function(callback)
+		print(callback)
+	end,
+})
+stav:CreateModule({
+	Name = 'Speed'
+})
+
+
+lib.library.CreateWindow('Stav.lua here')
+lib.library.CreateNotif('You have mail!', 6.7, 11422155687)
